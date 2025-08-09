b7d7d1296a395b63628d0017321de8b99343c623
Shahzadsultan3131 <shahzadsultan3131@gmail.com>
2025-08-09 05:50:49 +0500

    Updated Article

--- src/pages/EditorPage.ts
+++ src/pages/EditorPage.ts
@@ -2,11 +2,13 @@ import { expect, Page } from '@playwright/test';
 import { BasePage } from './BasePage';
 
 export class EditorPage extends BasePage {
-  constructor(page: Page) { super(page); }
+  constructor(page: Page) { 
+    super(page); 
+  }
 
   async createArticle(title: string, about: string, body: string, tags: string[]) {
     await this.page.fill('input[placeholder="Article Title"]', title);
-    await this.page.fill('input[placeholder="What's this article about?"]', about);
+    await this.page.fill("input[placeholder='What's this article about?']", about);
     await this.page.fill('textarea[placeholder="Write your article (in markdown)"]', body);
     await this.page.fill('input[placeholder="Enter tags"]', tags.join(' '));
     await this.page.click('button:has-text("Publish Article")');
